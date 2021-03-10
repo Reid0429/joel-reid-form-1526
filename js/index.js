5e896e8486f969e075c9848e277d266792179d41
Reid0429 <77335192+Reid0429@users.noreply.github.com>
2021-03-09 23:11:58 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+BEAUTIFY JAVASCRIPT BEAUTIFY JAVASCRIPT IN NEW WINDOW
+Beautified JavaScript:
 let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 let fn = document.getElementById('fullname');
 let em = document.getElementById('email');
@@ -6,48 +8,43 @@ let ms = document.getElementById('message');
 let b = document.getElementById('contact-form');
 
 
+function formValidator() {
 
-function formValidator(){
-    
-    let data = {};
-    let error = [];
-    
-    if(fn.value) {
-    data.fullname = fn.value;
-    } else {
-        error.push('full name is missing'); 
-    }
+	let data = {};
+	let error = [];
 
-    console.log(data, error);    
+	if (fn.value) {
+		data.fullname = fn.value;
+	} else {
+		error.push('full name is missing');
+	}
 
-    if(em.value) {
-        if (pattern.test(em.value)) {
-        data.email = em.value;
-        } else {
-        error.push('Email is invalid. Try again.');
-        }
-        } else {
-        error.push('Where is yor email?');
-            }
+	console.log(data, error);
 
-            
-        if(ms.value) {
-        data.message = ms.value;
-    } else {
-        error.push('Message name is missing');
+	if (em.value) {
+		if (pattern.test(em.value)) {
+			data.email = em.value;
+		} else {
+			error.push('Email is invalid. Try again.');
+		}
+	} else {
+		error.push('Where is yor email?');
+	}
+
+
+	if (ms.value) {
+		data.message = ms.value;
+	} else {
+		error.push('Message name is missing');
+	}
+
+
+	if (error.length === 0) {
+
+	} else {
+
+	}
+	console.log(data, error);
 }
-    
-  
-if (error.length === 0) { 
-
-} else {
-
-}
-console.log(data, error);    
-}
-                        
-
-
-b.addEventListener('click', formValidator);
 
 
